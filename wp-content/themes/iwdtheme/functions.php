bebc6591d3e8bbe2641e94499028df139862bbaa
Andry85 <shapovala@ukr.net>
2019-09-28 17:15:24 +0300

    added func

--- wp-content/themes/iwdtheme/functions.php
+++ wp-content/themes/iwdtheme/functions.php
@@ -222,3 +222,12 @@ add_action('admin_init', function() {
 });
 
 remove_filter('widget_text_content', 'wpautop');
+
+remove_action('load-update-core.php','wp_update_themes');
+add_filter('pre_site_transient_update_themes',create_function('$a', "return null;"));
+wp_clear_scheduled_hook('wp_update_themes');
+add_filter('pre_site_transient_update_core',create_function('$a', "return null;"));
+wp_clear_scheduled_hook('wp_version_check');
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+wp_clear_scheduled_hook( 'wp_update_plugins' );
